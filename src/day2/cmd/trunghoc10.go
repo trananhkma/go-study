550643afc31828f67380130998fa81130a24ddb9
trananhkma <trananhkma@gmail.com>
2018-12-17 14:42:30 +0700

    trunghoc10

---
+++ src/day2/cmd/trunghoc10.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    trunghoc10()
+}
+
+func trunghoc10() {
+    var n int
+    fmt.Scan(&n)
+    fmt.Println(1)
+
+    if n <= 1 {
+        return
+    }
+
+    for i:=2; i<=n; i++ {
+        for j:=1; j<=i; j++ {
+            fmt.Print(j)
+        }
+        for j:=i-1; j>=1; j-- {
+            fmt.Print(j)
+        }
+        fmt.Println()
+    }
+}
