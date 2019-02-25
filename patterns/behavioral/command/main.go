274a8c9eb5adf4d191fa4812a1238e9a75a1cc93
trananhkma <trananhkma@gmail.com>
2019-02-25 15:34:49 +0700

    update

--- patterns/behavioral/command/main.go
+++ patterns/behavioral/command/main.go
@@ -4,43 +4,52 @@ import "fmt"
 
 // Command
 type Order struct {
-	cook Cook
 	meal []string
 }
 
-func (o *Order) execute() {
-	fmt.Println("Ordering ", o.meal)
-	o.cook.prepare(o.meal)
+type Worker interface {
+	execute()
+}
+
+type Waiter struct {
+	Order
+}
+
+func (w *Waiter) execute() {
+	fmt.Println("This is Waiter")
+	fmt.Println("Get order from Customer")
+	fmt.Println("Send order to Cooker: ", w.Order.meal)
 }
 
 // Receiver
-type Cook struct{}
+type Cooker struct {
+	Order
+}
 
-func (c *Cook) prepare(meal []string) {
-	fmt.Println("Preparing meal: ", meal)
+func (c *Cooker) execute() {
+	fmt.Println("This is Cooker")
+	fmt.Println("Get order from Waiter")
+	fmt.Println("Cooking meal ", c.Order.meal)
 }
 
 // Invoker
-type Customer struct {
-	order Order
+type Customer struct{}
+
+func (c *Customer) mail_order(meal []string) {
+    order := Order{meal: meal}
+	waiter := Waiter{order}
+	cooker := Cooker{order}
+	execute_order(&waiter, &cooker)
 }
 
-func (c *Customer) mail_order(o *Order) {
-	o.execute()
+func execute_order(workers ...Worker) {
+	for _, w := range workers {
+		w.execute()
+	}
 }
 
 func main() {
-	c1 := new(Customer)
-	meals := []string{"Fish"}
-    meals = append(meals, "Cat")
-    meals = append(meals, "Dog")
-	
-	order1 := Order{meal: meals}
-    
-    c2 := new(Customer)
-    order2 := Order{meal: []string{"Chicken"}}
-    
-    
-    c2.mail_order(&order2)
-	c1.mail_order(&order1)
+	customer := new(Customer)
+	meal := []string{"Salat", "Soup", "Rice"}
+	customer.mail_order(meal)
 }
